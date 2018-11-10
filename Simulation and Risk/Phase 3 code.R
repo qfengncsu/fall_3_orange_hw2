e054bc40550641cc722044aafe6486bb38290d23
wjjenista <42449580+wjjenista@users.noreply.github.com>
2018-11-10 17:02:50 -0500

    Update Simulation and Risk/Phase 3 code.R

--- Simulation and Risk/Phase 3 code.R
+++ Simulation and Risk/Phase 3 code.R
@@ -2,8 +2,10 @@
 # Phase 3 code
 
 library(EnvStats)
+library(ks)
+library(dplyr)
 
-n=10000
+n=100000
 prop_producing <- rep(0,n)
 
 for (j in 1:n){
@@ -22,5 +24,17 @@ for (j in 1:n){
   prop_producing[j] = sum(producing)/length(producing)
 }
 
-hist(prop_producing)
+Density.p <- density(prop_producing, bw="SJ-ste")
+Density.p
 
+VaR5 <- qkde(0.05, fhat=kde(prop_producing, h=Density.p$bw))
+
+prop_producing <- as.data.frame(prop_producing)
+tail <- filter(prop_producing, prop_producing <= VaR5)
+
+CVaR5 = mean(tail$prop_producing)
+
+hist(prop_producing$prop_producing, main='Distribution of Proportion of Producing Wells', xlab='Proportion of Producing Wells')
+abline(v = VaR5, col="red", lwd=2)
+mtext("5% Value at Risk", at=VaR5, col="red")
+abline(v = CVaR5, col="blue")
